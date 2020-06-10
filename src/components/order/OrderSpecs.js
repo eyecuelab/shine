e741734e96e0ce9324ac43c240bc8c7543c8c273
Drake Wilcox <drakewilcox@gmail.com>
2020-06-10 15:02:58 -0700

    create boilerplate components for order process.

---
+++ src/components/order/OrderSpecs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const OrderSpecs = () => {
+  return (
+    <View>
+      <Text>Step 2: What is the typical Use?</Text>
+      <Text>How soon would you need them cleaned?</Text>
+    </View>
+  )
+}
+
+export default OrderSpecs;
