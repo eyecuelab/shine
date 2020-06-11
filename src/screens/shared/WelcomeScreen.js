13fe1cb927b09c956a64e0e82a4ff04f669b4284
Drake Wilcox <drakewilcox@gmail.com>
2020-06-11 14:55:07 -0700

    add styling to welcome screen. will move to the takePhoto component.

--- src/screens/shared/WelcomeScreen.js
+++ src/screens/shared/WelcomeScreen.js
@@ -7,19 +7,40 @@ export default WelcomeScreen = ({ navigation }) => {
     <>
       <Header title="Welcome" navigation={navigation} />
       <Container>
-        <Text>WelcomeScreen</Text>
+        <CameraView>
+          <Text>Camera View</Text>
+        </CameraView>
+        <Container style={{padding: 18}}>
+          <Text>Add a pair of shoes from your closet. Please follow the outlines as close as possible.</Text>
+        </Container>
+        
       </Container>
     </>
   )
 };  
 
+const CameraView = styled.View`
+  flex: 1.5;
+  background: #c6b18d;
+  align-self: stretch;
+  align-items: center;
+  justify-content: center;
+  border: black;
+  border-width: 1px;
+
+`
+
+
 const Container = styled.View`
+
+  background: white;
   flex: 1;
   align-items: center;
   justify-content: center;
 `;
 
 const Text = styled.Text`
+  text-align: center;
   color: black;
   font-size: 20px;
   font-weight: 500;
