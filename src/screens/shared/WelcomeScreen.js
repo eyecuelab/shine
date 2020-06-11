6c10bfcb31019d937e0cb2e390024e638d7f35cd
Drake Wilcox <drakewilcox@gmail.com>
2020-06-11 15:37:15 -0700

    move contents from WelcomeScreen to TakePhoto components so view can be changed onPress.

--- src/screens/shared/WelcomeScreen.js
+++ src/screens/shared/WelcomeScreen.js
@@ -1,49 +1,15 @@
 import React from 'react';
-import Header from "../../components/shared/Header";
 import styled from "styled-components";
-import { Button } from 'react-native-elements';
+import Header from "../../components/shared/Header";
+import TakePhoto from '../../components/order/TakePhoto'
+
 
 export default WelcomeScreen = ({ navigation }) => {
   return (
     <>
-      <Header title="Welcome" navigation={navigation} />
-      <Container>
-        <CameraView>
-          <Text>Camera View</Text>
-        </CameraView>
-        <Container style={{padding: 18}}>
-          <Text>Add a pair of shoes from your closet. Please follow the outlines as close as possible.</Text>
-          <Button
-            title="CONTINUE"
-            containerStyle={{paddingTop: 20, width: 350 }}
-            buttonStyle={{backgroundColor: 'black', height: 50, borderRadius: 7}}
-          />
-        </Container>
-      </Container>
+     <Header title="Welcome" navigation={navigation} />
+     <TakePhoto/>
     </>
   )
 };  
 
-const CameraView = styled.View`
-  flex: 2;
-  background: #c6b18d;
-  align-self: stretch;
-  align-items: center;
-  justify-content: center;
-  border: black;
-  border-width: 1px;
-`
-const Container = styled.View`
-
-  background: white;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Text = styled.Text`
-  text-align: center;
-  color: black;
-  font-size: 20px;
-  font-weight: 500;
-`;
