059e9479c6defea7245ce3bcc1db47c6d9fc4d94
Jieun Kang <jieunkang101@gmail.com>
2020-06-10 18:52:49 -0700

    add Header shared component

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import styled from "styled-components";
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ title, navigation }) {
@@ -9,31 +9,37 @@ export default function Header({ title, navigation }) {
   }
 
   return (
-    <View style={styles.header}>
-      <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-      <View>
-        <Text style={styles.headerText}>{title}</Text>
-      </View>
-    </View>
+    <SafeAreaView>
+    <Container>
+      <Icon>
+        <MaterialIcons name='menu' size={28} onPress={openMenu} />
+      </Icon>
+      <Text>{title}</Text>
+    </Container>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  header: {
-    width: '100%',
-    height: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headerText: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: '#333',
-    letterSpacing: 1,
-  },
-  icon: {
-    position: 'absolute',
-    right: 16,
-  }
-});
+const SafeAreaView = styled.SafeAreaView`
+  flex: 0.1;
+`;
+
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Icon = styled.View`
+  position: absolute;
+  right: 16px;
+`;
+
+const Text = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+  color: #333;
+  letter-spacing: 1;
+`;
