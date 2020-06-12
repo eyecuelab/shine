a91eba80c34eea8f084e5154cf2d579e68a46158
Drake Wilcox <drakewilcox@gmail.com>
2020-06-11 17:34:09 -0700

    change component to OrderSpecs for editing.

--- src/screens/shared/WelcomeScreen.js
+++ src/screens/shared/WelcomeScreen.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import styled from "styled-components";
+import React, { useState } from 'react';
 import Header from "../../components/shared/Header";
-import TakePhoto from '../../components/order/TakePhoto'
-
+import TakePhoto from '../../components/order/TakePhoto';
+import OrderSpecs from '../../components/order/OrderSpecs';
 
 export default WelcomeScreen = ({ navigation }) => {
+  const [screenView, setScreenView ] = useState('TakePhoto');
+
+
   return (
     <>
      <Header title="Welcome" navigation={navigation} />
-     <TakePhoto/>
+     {/* <TakePhoto onContinue={screenView}/> */}
+     <OrderSpecs />
     </>
   )
 };  
