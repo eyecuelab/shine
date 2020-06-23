38e4e5804f4f66ed22d92da8fa79f26c43bea7f1
Jieun Kang <jieunkang101@gmail.com>
2020-06-22 22:46:20 -0700

    make HTTP request for signIn function in AuthContext component

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import axios from "axios";
+
 
 const AuthContext = React.createContext();
 
@@ -59,11 +61,15 @@ export const AuthProvider = ({ children }) => {
   const authContext = React.useMemo(
     () => ({
       signIn: async data => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
-
+        axios.post(`https://shoeshine.herokuapp.com/login`, {
+        "email": data["username"], 
+        "password": data["password"]
+      })
+      .then((response) => {
+        console.log(response.data.data.attributes.token);
+      }, (error) => {
+        console.log(error);
+      });
         dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
       },
       signOut: () => dispatch({ type: 'SIGN_OUT' }),
@@ -78,6 +84,19 @@ export const AuthProvider = ({ children }) => {
     }),
     []
   );
+
+
+  // const logIn = () => axios.post(`https://shoeshine.herokuapp.com/login`, {
+  //   "email": "example@example.com", 
+  //   "password": "theshoe"
+  // })
+  // .then((response) => {
+  //   console.log(response);
+  // }, (error) => {
+  //   console.log(error);
+  // });
+
+  console.log("test", () => authContext.signIn());
    
   return (
     <AuthContext.Provider value={{authContext, state}}>
--- src/screens/shared/WelcomeScreen.js
+++ src/screens/shared/WelcomeScreen.js
@@ -3,14 +3,18 @@ import Header from '../../components/shared/Header';
 import axios from "axios";
 
 const WelcomeScreen = ({ navigation }) => {
- 
 
-  const logIn = () => axios.post(`https://shoeshine.herokuapp.com/login`, {
-    "email": "example@example.com", 
-    "password": "theshoe"
-  });
 
-  console.log(logIn);
+
+  // const logIn = () => axios.post(`https://shoeshine.herokuapp.com/login`, {
+  //   "email": "example@example.com", 
+  //   "password": "theshoe"
+  // })
+  // .then((response) => {
+  //   console.log(response);
+  // }, (error) => {
+  //   console.log(error);
+  // });
 
   return (
     <>
