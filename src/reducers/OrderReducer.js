216855265a3d9d75d28551ada07d241c3e556b20
Drake Wilcox <drakewilcox@gmail.com>
2020-06-24 14:44:18 -0700

    test ability to call reducer from mapStateToProps.

--- src/reducers/OrderReducer.js
+++ src/reducers/OrderReducer.js
@@ -3,16 +3,16 @@ import * as c from '../actions/types';
 const orders = (state = [], action) => {
   switch (action.type) {
     case c.ADD_ORDER:
-      console.log('ACTION: ', action);
+      console.log('ACTION: ', action.payload.orderInfo.image);
       return [
         ...state,
         {
           id: action.id,
-          image: action.payload.image,
-          shoeTypes: action.payload.shoeTypes,
-          timeFrame: action.payload.timeFrame,
-          note: action.payload.note,
-          price: action.payload.price,
+          image: action.payload.orderInfo.image,
+          shoeTypes: action.payload.orderInfo.shoeTypes,
+          timeFrame: action.payload.orderInfo.timeFrame,
+          note: action.payload.orderInfo.note,
+          price: action.payload.orderInfo.price,
         },
       ];
     default:
