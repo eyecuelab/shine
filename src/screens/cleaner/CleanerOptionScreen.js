4c9f6d3ad2c234c5f9585e52f55bf676bc449928
Drake Wilcox <drakewilcox@gmail.com>
2020-06-30 13:30:04 -0700

    add CleanerOptionScreen and set as default for cleaner tab.

---
+++ src/screens/cleaner/CleanerOptionScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import Header from '../../components/shared/Header';
+import { Button } from 'react-native-elements';
+
+const CleanerOptionScreen = ({ navigation }) => {
+  return (
+    <>
+      <Header title="SHINE" navigation={navigation} />
+      <Container>
+        <ImageArea>
+          <Image source={require('../../../assets/images/logo-outline.png')} />
+        </ImageArea>
+        <Button
+          title="APPLY TO BE A CLEANER"
+          containerStyle={{ paddingTop: 20, width: 350, marginVertical: 20 }}
+          buttonStyle={{
+            backgroundColor: 'black',
+            height: 50,
+            borderRadius: 7,
+          }}
+          onPress={() => navigation.navigate('Sign up')}
+        />
+        <Text>
+          Already have a cleaner account?{' '}
+          <TextLink onPress={() => navigation.navigate('Log in')}>
+            {' '}
+            Log in
+          </TextLink>
+        </Text>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+`;
+
+const ImageArea = styled.View`
+  width: 200px;
+  height: 200px;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
+
+const Text = styled.Text`
+  color: black;
+  font-size: 16px;
+`;
+
+const TextLink = styled.Text`
+  color: #cbb387;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export default CleanerOptionScreen;
