65837d728251ac3edf56819be7f2f1f84b2ce937
Drake Wilcox <drakewilcox@gmail.com>
2020-07-01 08:32:13 -0700

    use flex styling to put some form items on same line

--- src/screens/cleaner/CleanerApplicationScreen.js
+++ src/screens/cleaner/CleanerApplicationScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Input } from 'react-native-elements';
+import { StyleSheet, ScrollView, TextInput } from 'react-native';
 import styled from 'styled-components/native';
 // import PropTypes from 'prop-types';
 
@@ -16,38 +17,68 @@ const CleanerApplicationScreen = () => {
   // TO DO:
   // Add Icons, setState functions and flex styling. Check with API for correct information.
   return (
-    <Container>
-      <Input
-        label="Business"
-        labelStyle={{ fontSize: 20 }}
-        placeholder="Name"
-      />
-      <Input
-        label="Contact Name"
-        labelStyle={{ fontSize: 20 }}
-        placeholder="First Name"
-      />
-      <Input placeholder="Last Name" />
-      <Input label="Email" labelStyle={{ fontSize: 20 }} placeholder="Email" />
-      <Input
-        label="Phone Number"
-        labelStyle={{ fontSize: 20 }}
-        placeholder="Phone Number"
-      />
-      <Input
-        label="Address"
-        labelStyle={{ fontSize: 20 }}
-        placeholder="Street"
-      />
-      <Input placeholder="City" />
-      <Input placeholder="State" />
-      <Input placeholder="postalCode" />
-      <Input label="Bio" labelStyle={{ fontSize: 20 }} placeholder="Bio" />
-    </Container>
+    <ScrollView>
+      <Container>
+        <Input
+          label="Business"
+          labelStyle={{ fontSize: 20 }}
+          placeholder="Name"
+        />
+        <MultiLineInputs>
+          <Input
+            containerStyle={{ flex: 1 }}
+            label="Contact Name"
+            labelStyle={{ fontSize: 20 }}
+            placeholder="First Name"
+          />
+          <Input
+            containerStyle={{ flex: 1 }}
+            label=""
+            labelStyle={{ fontSize: 20 }}
+            placeholder="Last Name"
+          />
+        </MultiLineInputs>
+        <Input
+          label="Email"
+          labelStyle={{ fontSize: 20 }}
+          placeholder="Email"
+        />
+        <Input
+          label="Phone Number"
+          labelStyle={{ fontSize: 20 }}
+          placeholder="Phone Number"
+        />
+        <Input
+          label="Address"
+          labelStyle={{ fontSize: 20 }}
+          placeholder="Street"
+        />
+        <MultiLineInputs>
+          <Input placeholder="City" containerStyle={{ flex: 2 }} />
+          <Input placeholder="State" containerStyle={{ flex: 1 }} />
+        </MultiLineInputs>
+        <Input placeholder="Zip" />
+
+        <Input
+          label="Bio"
+          multiline={true}
+          numberOfLines={2}
+          labelStyle={{ fontSize: 20 }}
+          placeholder="Bio"
+        />
+      </Container>
+    </ScrollView>
   );
 };
 
+const MultiLineInputs = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-end;
+`;
+
 const Container = styled.View`
+  margin: 10px;
   flex: 1;
   align-items: center;
   justify-content: center;
