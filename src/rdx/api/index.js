370200fccd9d112be141abbfb01a57a28621771a
Drake Wilcox <drakewilcox@gmail.com>
2020-07-07 17:20:25 -0700

    create boilerplate for posting and order to the API.

--- src/rdx/api/index.js
+++ src/rdx/api/index.js
@@ -1,3 +1,5 @@
+import { put } from 'redux-saga/effects';
+
 const fetchOrders = async (token) => {
   console.log(token);
   const response = await fetch('http://127.0.0.1:8080/orders', {
@@ -9,11 +11,29 @@ const fetchOrders = async (token) => {
   const result = await response.json();
   console.log('API CALL: ', result.data);
 
-  if (response.statusCode > 400) {
+  if (response.statusCode >= 400) {
     throw new Error(result.data.errors);
   } else {
     return result.data;
   }
 };
 
-export { fetchOrders };
+const postOrder = (token) => {
+  const POST_ORDER_ENDPOINT = 'http://127.0.0.1:8080/orders';
+  const parameters = {
+    method: 'POST',
+    headers: {
+      Authorization: token,
+    },
+  };
+
+  return fetch(POST_ORDER_ENDPOINT, parameters).then((response) => {
+    if (response.ok && response.status === 200) {
+      return response;
+    } else {
+      throw new Error(response.json());
+    }
+  });
+};
+
+export { fetchOrders, postOrder };
