6a0bd93d0488b6e5b2affc7c0e7bfad0c84e903a
Jieun Kang <jieunkang101@gmail.com>
2020-07-15 21:06:15 -0700

    add DateTimePicker into QuotableOrderDetailScreen

--- src/screens/cleaner/QuotableOrderDetailScreen.js
+++ src/screens/cleaner/QuotableOrderDetailScreen.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  View,
+  Button,
+  Platform,
+  TouchableOpacity,
+  Keyboard,
+  TouchableHighlight,
+  Alert,
+  Modal,
+} from 'react-native';
 import { connect } from 'react-redux';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import ScrollViewContailner from '../../components/shared/ScrollViewContainer';
 import ShoePhoto from '../../components/shared/ShoePhoto';
 import AddOnSwitch from '../../components/order/AddOnSwitch';
@@ -9,13 +20,36 @@ import * as actions from '../../rdx/actions';
 
 const QuotableOrderDetailScreen = ({ route }) => {
   const item = route.params;
-  console.log('DETAIL', item);
+  // console.log('DETAIL', item);
+  const today = new Date();
+  const [expireDate, setExpireDate] = useState(new Date(today));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  console.log('DATE', expireDate);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || expireDate;
+    setShow(Platform.OS === 'ios');
+    setExpireDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(!show);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
 
   return (
     <ScrollViewContailner>
       {ShoePhoto(item.image)}
       <Container>
-        <Text>{item.attributes.time_frame}</Text>
         <SwitchTextContainer>
           <SwitchText>ADD POLISH</SwitchText>
           <SwitchText>ADD RAIN PROTECTION</SwitchText>
@@ -36,13 +70,28 @@ const QuotableOrderDetailScreen = ({ route }) => {
           />
         </SwitchContainer>
       </Container>
+
+      <View>
+        <Button onPress={showDatepicker} title="Set Expire Date" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Set Expire Time" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={expireDate}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </ScrollViewContailner>
   );
 };
 
 const Container = styled.View`
-  width: 100%;
-  height: 100%;
   align-items: center;
   justify-content: center;
   flex-direction: row;
