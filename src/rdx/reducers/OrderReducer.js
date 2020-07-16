72cc58b93bbace391fc81169946f4af25930741f
Drake Wilcox <drakewilcox@gmail.com>
2020-07-16 12:09:21 -0700

    remove uneeded items from reducer.

--- src/rdx/reducers/OrderReducer.js
+++ src/rdx/reducers/OrderReducer.js
@@ -7,58 +7,7 @@ const orderReducer = (state = [], action) => {
       return [];
     case types.LOAD_ORDERS_SUCCESS:
       return [...action.payload];
-    // case types.ADD_ORDER:
-    //   return [
-    //     ...state,
-    //     {
-    //       uuid: action.payload.uuid,
-    //       image: action.payload.image,
-    //       shoeTypes: action.payload.shoeTypes,
-    //       timeFrame: action.payload.timeFrame,
-    //       note: action.payload.note,
-    //       estimatedPrice: action.payload.price,
-    //       addOns: {
-    //         polish: false,
-    //         rainProtection: false,
-    //         replaceLaces: false,
-    //       },
-    //       orderAddress: {
-    //         streetAddress: '',
-    //         aptNumber: '',
-    //         zipCode: '',
-    //       },
-    //       requestCompleted: false,
-    //       createdAt: null,
-    //       updatedAt: null,
-    //       publishedAt: null,
-    //       quoteAcceptedAt: null,
-    //       cleanerId: null,
-    //       userId: null,
-    //       completedAt: null,
-    //       deletedAt: null,
-    //     },
-    //   ];
-    // case types.ADD_ADD_ONS:
-    //   return state.map((item) => {
-    //     if (item.uuid === action.uuid) {
-    //       return {
-    //         ...item,
-    //         addOns: action.payload,
-    //       };
-    //     }
 
-    //     return item;
-    //   });
-    // case types.ADD_ORDER_ADDRESS:
-    //   return state.map((item) => {
-    //     if (item.uuid === action.uuid) {
-    //       return {
-    //         ...item,
-    //         orderAddress: action.payload,
-    //       };
-    //     }
-    //     return item;
-    //   });
     case types.REQUEST_COMPLETE:
       return state.map((item) => {
         if (item.uuid === action.uuid) {
