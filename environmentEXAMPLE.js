78d8cbbc6ce812a71c2457a7e45796cf695b7227
Drake Wilcox <drakewilcox@gmail.com>
2020-07-21 16:22:31 -0700

    change env example for pushing to testflight.

--- environmentEXAMPLE.js
+++ environmentEXAMPLE.js
@@ -1,32 +1,30 @@
 /* eslint-disable no-undef */
 import Constants from 'expo-constants';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
-const localhost = Platform.OS === 'ios' ? 'localhost:8080' : '10.0.2.2:';
+// const localhost = Platform.OS === 'ios' ? 'http://127.0.0.1:8080' : '10.0.2.2:';
+
+const remotehost = 'https://shoeshine.herokuapp.com';
 
 const ENV = {
   dev: {
-    apiUrl: localhost,
-    amplitudeApiKey: null,
-    AWSAccessKeyId: 'YOUR AWS ACCESSKEY HERE',
-    AWSSecretKey: 'YOUR AWS SECRET KEY HERE',
+    apiUrl: remotehost,
+    AWSAccessKeyId: 'AWS ACCESS KEY HERE',
+    AWSSecretKey: 'AWS SECRET HERE',
   },
   staging: {
-    apiUrl: '[your.staging.api.here]',
-    amplitudeApiKey: '[Enter your key here]',
-    // Add other keys you want here
+    apiUrl: remotehost,
+    AWSAccessKeyId: 'AWS ACCESS KEY HERE',
+    AWSSecretKey: 'AWS SECRET HERE',
   },
   prod: {
-    apiUrl: '[your.production.api.here]',
-    amplitudeApiKey: '[Enter your key here]',
-    // Add other keys you want here
+    apiUrl: remotehost,
+    AWSAccessKeyId: 'AWS ACCESS KEY HERE',
+    AWSSecretKey: 'AWS SECRET HERE',
   },
 };
 
 const getEnvVars = (env = Constants.manifest.releaseChannel) => {
-  // What is __DEV__ ?
-  // This variable is set to true when react-native is running in Dev mode.
-  // __DEV__ is true when run locally, but false when published.
   if (__DEV__) {
     return ENV.dev;
   } else if (env === 'staging') {
