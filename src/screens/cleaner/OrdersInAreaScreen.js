73efbb513262fed37d403d5f9e933766e77ed460
Jieun Kang <jieunkang101@gmail.com>
2020-07-20 18:41:08 -0700

    update OrderStatusScreen with cleaner and quote info from api response

--- src/screens/cleaner/OrdersInAreaScreen.js
+++ src/screens/cleaner/OrdersInAreaScreen.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import { connect } from 'react-redux';
 import ScrollViewContainer from '../../components/shared/ScrollViewContainer';
 import styled from 'styled-components/native';
