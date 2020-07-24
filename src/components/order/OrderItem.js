7f0c274d0a630dcacc2b778724314f0ca6c7751f
Drake Wilcox <drakewilcox@gmail.com>
2020-07-24 01:10:25 -0700

    add a static status label for items in order list, functionality to be added later.

--- src/components/order/OrderItem.js
+++ src/components/order/OrderItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, Text } from 'react-native';
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
 
@@ -7,16 +7,37 @@ const { width: WIDTH, height: HEIGHT } = Dimensions.get('window');
 
 const ListItem = ({ order }) => {
   return order.attributes.image_url === null ? (
-    <ImageArea
+    <Image
       // eslint-disable-next-line no-undef
       source={require('../../../assets/images/shoes_placeholder.png')}
     />
   ) : (
-    <ImageArea source={{ uri: order.attributes.image_url }} />
+    <ImageArea
+      source={{ uri: order.attributes.image_url }}
+      imageStyle={{ borderRadius: 25 }}
+    >
+      <StatusText>STATUS</StatusText>
+    </ImageArea>
   );
 };
 
-const ImageArea = styled.Image`
+const StatusText = styled.Text`
+  font-family: Raleway-Bold;
+  background-color: rgba(50, 168, 82, 0.5)
+  color: white;
+
+  font-size: 16px;
+  padding: 5px;
+`;
+
+const ImageArea = styled.ImageBackground`
+  width: ${WIDTH / 2.4}px;
+  height: ${HEIGHT / 5}px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.Image`
   width: ${WIDTH / 2.4}px;
   height: ${HEIGHT / 5}px;
   border-radius: 15px;
